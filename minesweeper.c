e988d390f3ab2e60d82b465508c438a375b38967
Tin Kranželić <41866771+tkranza@users.noreply.github.com>
2018-07-29 22:17:57 +0200

    Add files via upload

--- minesweeper.c
+++ minesweeper.c
@@ -1,20 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void pros(int a[][20],int u, int v,int m,int n){
+
+int b = 0, i2,j2;
+
+for(i2 = (u-1>0 ? u-1 : 0) ; i2<(u+2<m ? u+2 : m); i2++)
+    for(j2 = (v-1>0 ? v-1 : 0); j2<(v+2<n ? v+2 : n); j2++){
+            if(a[i2][j2] == -2) b++;
+            }
+
+a[u][v] = b;
+
+if(b != 0) return;
+
+if(b == 0){
+    for(i2 = (u-1>0 ? u-1 : 0) ; i2<(u+2<m ? u+2 : m); i2++)
+        for(j2 = (v-1>0 ? v-1 : 0); j2<(v+2<n ? v+2 : n); j2++){
+            if(a[i2][j2] != 0) pros(a,i2,j2,m,n);
+            }
+}
+}
+
 
 int main(void){
 
-int m, n, a[20][20], k, i, j, kp, u,v, i2, j2, b, b2=0;
+int m, n, a[20][20], k, i, j, kp, u,v, i2, j2, b, b2;
 do{
 printf(" unesi 1. dimenziju polja : ");
 scanf(" %d", &m);
-if(m<=0 || m>20) printf(" neispravan unos\n");
-} while(m<=0 || m>20);
+if(m<=0) printf(" neispravan unos\n");
+} while(m<=0);
 do{
 printf(" unesi 2. dimenziju polja : ");
 scanf(" %d", &n);
-if(n<=0 || n>20) printf(" neispravan unos\n");
-} while(n<=0 || n>20);
+if(n<=0) printf(" neispravan unos\n");
+} while(n<=0);
 do{
 printf(" koliko mina na polju ? : ");
 scanf(" %d", &k);
@@ -60,7 +81,6 @@ printf(" GAME OVER");
 return;
 }
 if(a[u][v] == -1){
-        b2++;
         b = 0;
         for(i2 = (u-1>0 ? u-1 : 0) ; i2<(u+2<m ? u+2 : m); i2++)
         for(j2 = (v-1>0 ? v-1 : 0); j2<(v+2<n ? v+2 : n); j2++){
@@ -68,6 +88,16 @@ if(a[u][v] == -1){
             }
             a[u][v] = b;
 
+if(b==0) pros(a,u,v,m,n);
+
+b2 = 0;
+
+for(i2 = 0; i2<m; i2++)
+        for(j2 = 0; j2<n; j2++){
+  if(a[i2][j2] != -1 && a[i2][j2] != -2) b2++;
+}
+
+if(b2 != m*n-k){
 for(i2 = 0; i2<m; i2++){
         for(j2 = 0; j2<n; j2++){
                 if(a[i2][j2] != -1 && a[i2][j2] != -2){
@@ -77,7 +107,9 @@ for(i2 = 0; i2<m; i2++){
         }
         printf(" \n");
 }
-if(b2 == m*n - k) {
+}
+
+if(b2 == m*n-k) {
 for(i=0; i<m; i++){
 for(j=0; j<n; j++){
     if(a[i][j] != -1 && a[i][j] != -2){
